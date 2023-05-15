6598a3567e58e6551fe4f0e93a206600c300e138
Nik-Sch <mail@niklas-schelten.de>
2023-05-15 12:13:04 +0200

    tmux

--- bin/change_theme.py
+++ bin/change_theme.py
@@ -3,7 +3,6 @@ import argparse
 import json
 import subprocess
 from pathlib import Path
-from time import sleep
 
 
 def set_bg(name: str):
@@ -17,11 +16,7 @@ def set_bg(name: str):
         ]
     )
 
-
 def set_theme(name: str):
-    # subprocess.call(
-    #     ["xfconf-query", "-c", "xsettings", "-np", "/Net/ThemeName", "-s", name]
-    # )
     subprocess.call(
         ["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", name]
     )
@@ -59,18 +54,16 @@ def set_vscode(name: str):
 
 
 def go_dark():
-    # set_theme("Adwaita-dark")
     set_color("prefer-dark")
-    set_vscode("Atom One Dark")
-    set_bg(f"{Path.home()}/configGit/falcon.jpg")
+    set_vscode("One Dark Pro")
+    set_bg(f"{Path.home()}/dark.jpg")
     set_kitty("Atom")
 
 
 def go_light():
-    # set_theme("Adwaita")
     set_color("prefer-light")
     set_vscode("Atom One Light")
-    set_bg(f"{Path.home()}/configGit/light.jpg")
+    set_bg(f"{args.background_path}/light.jpg")
     set_kitty("Atom One Light")
 
 
@@ -95,13 +88,15 @@ try:
 except Exception as e:
     print(e)
     print("Error during settings read -> default dark")
+
 print(f"Detected {MODE} mode")
 parser = argparse.ArgumentParser(
     prog="go mode",
     description="Sets/Toggles light dark mode",
 )
-parser.add_argument('-n', '--no-toggle', action='store_true');
-args = parser.parse_args();
+parser.add_argument('-n', '--no-toggle', action='store_true')
+parser.add_argument('-b', '--background-path', default=Path.home())
+args = parser.parse_args()
 if not args.no_toggle:
     toggle()
 if MODE == "dark":
