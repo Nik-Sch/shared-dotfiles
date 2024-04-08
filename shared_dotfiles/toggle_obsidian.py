526ce50243b11f50534e4f7dabe7dac89f024e20
Anton Schulte <anton.schulte@hhi.fraunhofer.de>
2024-04-08 13:49:43 +0200

    fix launching

--- shared_dotfiles/toggle_obsidian.py
+++ shared_dotfiles/toggle_obsidian.py
@@ -1,25 +1,23 @@
 import subprocess
 from shared_dotfiles.toggle_app import toggle_app
 
+OBSIDIAN_BINARY = "obsidian"
+OBSIDIAN_CLASS = "obsidian"
+
 
 def check_is_running() -> bool:
-    return (
-        len(
-            subprocess.run(
-                ["ps", "ux"],
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-            .stdout.strip()
-            .split()
+    try:
+        subprocess.run(
+            ["xdotool", "search", "--class", OBSIDIAN_CLASS],
+            check=True,
+            capture_output=True,
+            text=True,
         )
-        > 0
-    )
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 def run():
-    OBSIDIAN_BINARY = "obsidian"
-    OBSIDIAN_CLASS = "obsidian"
 
     toggle_app(OBSIDIAN_BINARY, OBSIDIAN_CLASS, check_is_running)
